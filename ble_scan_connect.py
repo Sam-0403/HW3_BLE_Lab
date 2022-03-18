b1c204969bb6442254e3e0e73b266a0187a0412b
鄭至盛 <sam22187212@gmail.com>
2022-03-18 19:57:36 +0800

    Test

--- ble_scan_connect.py
+++ ble_scan_connect.py
@@ -72,11 +72,11 @@ try:
 
     if enableNotify:
         # setup to enable notifications
-        dev.writeCharacteristic(cccd, b"\x01")
+        dev.writeCharacteristic(cccd, b"\x01\x00")
         print("Enable notifications......")
     if enableIndicate:
         # setup to enable indications
-        dev.writeCharacteristic(cccd, b"\x02")
+        dev.writeCharacteristic(cccd, b"\x02\x00")
         print("Enable indications......")
 
     time.sleep(1.0)
